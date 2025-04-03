567c1e03eb4faed8aed4dad25cb35b02ac438abe
Wooksang Ryu <today8934@gmail.com>
2025-04-04 06:51:19 +0900

    feature: create get_node function

--- 2nd_week/02_02_append_linked_list.py
+++ 2nd_week/02_02_append_linked_list.py
@@ -20,6 +20,16 @@ class LinkedList:
             if (node.next == None): break
             node = node.next
 
+    def get_node(self, index):
+        node = self.head
+        number = 0
+        while(True):
+            if (node == None): return "Out of Index"
+            if (number == index):
+                return node.data
+            node = node.next
+            number = number + 1
+
 linked_list = LinkedList(5)
 
 linked_list.append(3)
@@ -27,6 +37,7 @@ linked_list.append(4)
 linked_list.append(12)
 
 linked_list.print_all()
+print(linked_list.get_node(4))
 
 
     
