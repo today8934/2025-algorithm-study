3f67a828c606a0c96a73590cc5bd3ae94a548f8e
Wooksang Ryu <today8934@gmail.com>
2025-04-04 07:06:44 +0900

    feature create add_node function

--- 2nd_week/02_02_append_linked_list.py
+++ 2nd_week/02_02_append_linked_list.py
@@ -30,6 +30,25 @@ class LinkedList:
             node = node.next
             number = number + 1
 
+    def add_node(self, data, index):
+        if (index < 0): return "Out of Index"
+        if (index == 0):
+            tmp_node = self.head
+            self.head = Node(data)
+            self.head.next = tmp_node
+            return
+        node = self.head
+        number = 0
+        while(True):
+            if (node == None): return "Out of Index"
+            if (number == index - 1):
+                tmp_node = node.next
+                node.next = Node(data)
+                node.next.next = tmp_node
+                return
+            node = node.next
+            number = number + 1
+
 linked_list = LinkedList(5)
 
 linked_list.append(3)
@@ -38,6 +57,8 @@ linked_list.append(12)
 
 linked_list.print_all()
 print(linked_list.get_node(4))
+linked_list.add_node(7, 5)
+linked_list.print_all()
 
 
     
